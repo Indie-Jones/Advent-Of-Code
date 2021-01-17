6ba7f8b9c7db70779b1e58b7423c57a30329d404
Indie-Jones <Jonas.Bruer@gmx.de>
2021-01-17 22:58:08 +0100

    Day 2 - Part 2 (Python)
    My Solution for AOC 2020 Day 2 part 2 in python

--- Day_02/main.py
+++ Day_02/main.py
@@ -12,6 +12,28 @@ def correctPasswordCount(input):
 
     return correct_count
 
+def newCorrPassCount(input):
+    correct_count = 0
+
+    for e in input:
+        symbol_appearances = 0
+        if (e["min_count"] > 0 and
+                e["min_count"] <= len(e["password"]) and
+                e["symbol"] == e["password"][e["min_count"] - 1]):
+
+            symbol_appearances += 1
+
+        if (e["max_count"] > 0 and
+                e["max_count"] <= len(e["password"]) and
+                e["symbol"] == e["password"][e["max_count"] - 1]):
+
+            symbol_appearances += 1
+
+        if symbol_appearances == 1:
+            correct_count += 1
+
+    return correct_count
+
 ### parse input
 
 input_file = open("input.txt", "r")
@@ -29,3 +51,6 @@ if DEBUG:
 
 # Output for part 1
 print(correctPasswordCount(input))
+
+# Output for part 2
+print(newCorrPassCount(input))
